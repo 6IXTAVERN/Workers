05b38c895b5ddbd052dfec423089ebf4f3d61ade
6IXTAVERN <6ixtavern@mail.ru>
2023-09-23 23:45:03 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Workers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
