0998a7352203923ce3b57483b9863eba0ab63952
6IXTAVERN <6ixtavern@mail.ru>
2023-10-16 01:48:54 +0300

    Some logical changes

--- Program.cs
+++ Program.cs
@@ -1,27 +1,57 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Workers.Areas.Identity.Data;
+using Workers.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AppDatabaseContextConnection") ?? throw new InvalidOperationException("Connection string 'AppDatabaseContextConnection' not found.");
+
+builder.Services.AddDbContext<AppDatabaseContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDatabaseContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+// MySQL Database Connection
+//var getConnectionStringName = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.Services.AddDbContext<ApplicationDatabaseContext>(options => 
+//    options.UseMySql(getConnectionStringName, ServerVersion.AutoDetect(getConnectionStringName)));
+//
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<WorkersContext>();
+//
+//builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<WorkersContext>();
+
+
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "about",
+    pattern: "{controller=Home}/{action=About}/{id?}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
